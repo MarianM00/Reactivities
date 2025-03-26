318479ec8e2e588122f6640cd632e04a39f3bebe
MarianM00 <marin_puiu99@yahoo.com>
2025-03-26 14:55:49 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,14 +10,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3001", "https://localhost:3001"));
+
 app.MapControllers();
 
-using var scope  = app.Services.CreateScope();
+using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 try
